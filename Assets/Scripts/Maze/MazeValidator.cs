6a584ba41a20f5f3d3c3551c4bec10e8f4dee9d7
AnJung wasabi-neko <wasabi.neko.owo@gmail.com>
2023-07-09 17:48:27 +0800

    Feat: change bft to shortest path

--- Assets/Scripts/Maze/MazeValidator.cs
+++ Assets/Scripts/Maze/MazeValidator.cs
@@ -23,12 +23,18 @@ public class MazeValidator
         WallState[] directStatArr = {WallState.UP, WallState.LEFT, WallState.DOWN, WallState.RIGHT};
         Vector2Int[] directVecArr = {new Vector2Int(0, 1), new Vector2Int(-1, 0), new Vector2Int(0, -1), new Vector2Int(1, 0)};
         Vector2Int[,] traceBackMap = new Vector2Int[rows, cols];
-        int[,]  isVisited = new int[rows, cols];        // init to 0 by default
+        int[,] steps = new int[rows, cols];        // init to 0 by default
         Queue<Vector2Int> queue = new Queue<Vector2Int>();
 
-        // start dfs
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                steps[i, j] = -1;
+            }
+        }
+
+        // start bfs
         queue.Enqueue(currentPos);     
-        isVisited[currentPos.x, currentPos.y] = 1;
+        steps[currentPos.x, currentPos.y] = 0;
 
         while (queue.Count != 0) {
             Vector2Int pos = queue.Dequeue();
@@ -38,14 +44,18 @@ public class MazeValidator
                 break;
             }
 
+            int newSteps = steps[pos.x, pos.y] + 1;
             for (int i = 0; i < 4; i++) {
                 WallState state = directStatArr[i];
                 Vector2Int direction = directVecArr[i];
                 Vector2Int newPos = pos + direction;
+                int targetSteps = steps[newPos.x, newPos.y];
 
                 // no wall AND newPos not visited
-                if (!maze[pos.x, pos.y].HasFlag(state) && isVisited[newPos.x, newPos.y] == 0) {
-                    isVisited[newPos.x, newPos.y] = 1;
+                if (!maze[pos.x, pos.y].HasFlag(state) 
+                        && (newSteps < targetSteps || targetSteps == -1)) {
+
+                    steps[newPos.x, newPos.y] = newSteps;
                     traceBackMap[newPos.x, newPos.y] = pos;
                     queue.Enqueue(newPos);
                 }
@@ -53,7 +63,7 @@ public class MazeValidator
         }
 
         // path not found
-        if (isVisited[destPos.x, destPos.y] == 0) {
+        if (steps[destPos.x, destPos.y] == -1) {
             return false;
         }
 
